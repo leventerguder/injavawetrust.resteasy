7cc48990f9bab7172c731f75e55b8aac5d9e5ad1
levent <levent@ekmekTeknesi.ekmekTeknesi>
2016-09-04 16:44:29 +0300

    applicationpath

---
+++ injavawetrust.resteasy.annotation/src/service/RegisterApplication.java
@@ -0,0 +1,30 @@
+package service;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import controller.RestMessageController;
+
+@ApplicationPath("/root-path")
+
+//@ApplicationPath("/") OK
+//@ApplicationPath("/*") Don't use /*
+//@ApplicationPath("/root-path/*") Don't use /*
+
+public class RegisterApplication extends Application{
+
+	   private Set<Object> singletons = new HashSet<Object>();
+	   
+	   public RegisterApplication() {
+	      singletons.add(new RestMessageController());
+	   }
+	   
+	   @Override
+	   public Set<Object> getSingletons() {
+	      return singletons;
+	   }
+}
