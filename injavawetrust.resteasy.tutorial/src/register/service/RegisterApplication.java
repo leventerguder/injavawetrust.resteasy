60a9acc02997ea09e58aed5d2d72eb29722afb69
leventerguder <erguder.levent@gmail.com>
2016-09-25 11:40:44 +0300

    @MatrixParam & PathSegment

--- injavawetrust.resteasy.tutorial/src/register/service/RegisterApplication.java
+++ injavawetrust.resteasy.tutorial/src/register/service/RegisterApplication.java
@@ -1,6 +1,5 @@
 package register.service;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,18 +7,19 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
-public class RegisterApplication extends Application{
+public class RegisterApplication extends Application {
 
-	   private Set<Object> singletons = new HashSet<Object>();
-	   
-	   public RegisterApplication() {
-	      singletons.add(new _01.path.service.CustomerResource());
-	      singletons.add(new _02.pathParam.service.CustomerResource());
-	      singletons.add(new _02.pathParam.service.OrderResource());
-	   }
-	   
-	   @Override
-	   public Set<Object> getSingletons() {
-	      return singletons;
-	   }
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public RegisterApplication() {
+		singletons.add(new _01.path.service.CustomerResource());
+		singletons.add(new _02.pathParam.service.CustomerResource());
+		singletons.add(new _03.pathSegment.service.CarResource());
+		singletons.add(new _04.matrixParam.service.CarResource());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
 }
