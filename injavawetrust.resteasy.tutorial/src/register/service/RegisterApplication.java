59d88abf2791a942422655318ed42a5b2242c60c
leventerguder <erguder.levent@gmail.com>
2016-10-19 21:30:15 +0300

    change package

--- injavawetrust.resteasy.tutorial/src/register/service/RegisterApplication.java
+++ injavawetrust.resteasy.tutorial/src/register/service/RegisterApplication.java
@@ -24,7 +24,7 @@ public class RegisterApplication extends Application {
 		singletons.add(new _10.defaulValue.service.CarResource());
 		singletons.add(new _11.encoded.service.EmployeeResource());
 		singletons.add(new _12.context.service.ContextResource());
-		singletons.add(new _12.standardEntityProvider.service.ProviderResource());
+		singletons.add(new _13.standardEntityProvider.service.ProviderResource());
 		singletons.add(new _13.jaxb.xml.service.PersonResource());
 		singletons.add(new _13.jaxb.xml.service.CustomerResource());
 		singletons.add(new _14.json.service.CustomerResource());
