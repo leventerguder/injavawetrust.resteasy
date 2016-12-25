f82ac4c08889dea0a202f7e4479d126f6f2d51ee
leventerguder <erguder.levent@gmail.com>
2016-12-25 14:19:37 +0200

    review

--- injavawetrust.resteasy.tutorial/src/_09/beanParam/service/ProductResource.java
+++ injavawetrust.resteasy.tutorial/src/_09/beanParam/service/ProductResource.java
@@ -10,25 +10,22 @@ import _09.beanParam.model.Product;
 public class ProductResource {
 
 	@GET
-	@Path("/getProductById/{category : category}")
-	// if we use @PathParam , have to declare in path. otherwise we get
-	// exception.
-	// this is not required for jersey implementation.
-	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductById/category?id=100
+	@Path("/getProductById/{category}")
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductById/computer?id=100
 	public String getProductById(@BeanParam Product product) {
 		return "getProductById is called. <br/> " + product.toString();
 	}
 
 	@GET
-	@Path("/getProductsByBrand/{category : category}")
-	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductsByBrand/category;brand=acer
+	@Path("/getProductsByBrand/{category}")
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductsByBrand/computer;brand=acer
 	public String getProductsByBrand(@BeanParam Product product) {
 		return "getProductsByBrand is called. <br/>" + product.toString();
 	}
-	
+
 	@GET
 	@Path("/getProductsByCategoryAndBrand/{category}")
-	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductsByCategoryAndBrand/laptop;brand=acer;
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/product-beanParam/getProductsByCategoryAndBrand/laptop;brand=acer;price=123.45;
 	public String getProductsByCategoryAndBrand(@BeanParam Product product) {
 		return "getProductsByCategoryAndBrand is called. <br/>" + product.toString();
 	}
