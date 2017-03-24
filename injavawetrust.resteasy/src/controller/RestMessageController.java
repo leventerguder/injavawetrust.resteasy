c0e7ff864fed8262759d0d254b26864a6091b997
leventerguder <erguder.levent@gmail.com>
2017-03-24 23:23:07 +0300

    upgrade dependency

--- injavawetrust.resteasy/src/controller/RestMessageController.java
+++ injavawetrust.resteasy/src/controller/RestMessageController.java
@@ -6,6 +6,16 @@ import javax.ws.rs.Path;
 @Path("/messages")
 public class RestMessageController {
 
+	
+	// http://localhost:8080/injavawetrust.resteasy/messages/message
+	
+	// web-mapping-prefix.xml
+	// http://localhost:8080/injavawetrust.resteasy/resteasy/messages/message
+
+	// web-filter-dispatcher.xml
+	// change file name web-filter-dispatcher.xml to web.xml for testing
+	// http://localhost:8080/injavawetrust.resteasy/restfilter/messages/message
+	
 	@GET
 	@Path("/message")
 	public String getMessage() {
