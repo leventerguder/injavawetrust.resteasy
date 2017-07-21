358bacc2a5f1b7427aab150b0f20697478a656a0
leventerguder <erguder.levent@gmail.com>
2017-07-21 22:02:41 +0300

    encoded review

--- injavawetrust.resteasy.tutorial/src/_11/encoded/service/EmployeeResource.java
+++ injavawetrust.resteasy.tutorial/src/_11/encoded/service/EmployeeResource.java
@@ -10,40 +10,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 @Path("/employee-encoded")
-@Encoded
+// @Encoded
 public class EmployeeResource {
-
-	@Path("/getEmployee")
-	@GET
-	//http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployee?name=['levent']
-	//http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployee?name=['öÖğıİçÇöÖğĞ']
-	public String getEmployeeByName(@QueryParam("name") String name) {
-		
-		String message = "getEmployeeByName is called.";
-		return message + " name : " + name;
-
-	}
 
 	@Path("/getEmployeeEncoded")
 	@GET
-	//http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncoded?name=['levent']
-	//http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncoded?name=['öÖğıİçÇöÖğĞ']
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncoded?name=['levent']
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncoded?name=['öÖğıİçÇöÖğĞ']
+	// Using this annotation on a method will disable decoding for all parameters.
 	public String getEmployeeByNameEncoded(@Encoded @QueryParam("name") String name)
 			throws UnsupportedEncodingException {
-		
+
 		String message = "getEmployeeByNameEncoded is called. <br/>";
-		String encodedName = URLDecoder.decode(name, "UTF-8");
-		return message + " not encoded :" + name + "<br/>encoded : " + encodedName;
+		String decodedName = URLDecoder.decode(name, "UTF-8");
+		return message + " decodedName :" + decodedName + "<br/>encoded : " + name;
 
 	}
-	
+
 	@Path("/getEmployeeEncodedUTF8")
 	@GET
 	@Produces("text/html; charset=UTF-8")
-	//http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncodedUTF8?name=['öÖğıİçÇöÖğĞ']
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/employee-encoded/getEmployeeEncodedUTF8?name=['öÖğıİçÇöÖğĞ']
 	public String getEmployeeByNameEncodedUTF8(@Encoded @QueryParam("name") String name)
 			throws UnsupportedEncodingException {
-		
+
 		String message = "getEmployeeByNameEncodedUTF8 is called. <br/>";
 		String encodedName = URLDecoder.decode(name, "UTF-8");
 		return message + " not encoded :" + name + "<br/>encoded : " + encodedName;
