a91c7be9bc54b695968c5e6f1819991282e3096c
leventerguder <erguder.levent@gmail.com>
2017-07-29 12:26:41 +0300

    sublocator

--- injavawetrust.resteasy.tutorial/src/_01/path/service/subresource/AddressResource.java
+++ injavawetrust.resteasy.tutorial/src/_01/path/service/subresource/AddressResource.java
@@ -3,8 +3,11 @@ package _01.path.service.subresource;
 import javax.ws.rs.GET;
 
 public class AddressResource {
+	
 	@GET
 	public String getAddress() {
+		System.out.println("AddressResource#getAddress is called.");
 		return "Istanbul";
 	}
+	
 }
