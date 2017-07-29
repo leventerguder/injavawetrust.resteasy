f71ca73787b790e66bbc237184484ba96dc0cad9
leventerguder <erguder.levent@gmail.com>
2017-07-29 17:02:35 +0300

    review

--- injavawetrust.resteasy.tutorial/src/_13/standardEntityProvider/service/ProviderResource.java
+++ injavawetrust.resteasy.tutorial/src/_13/standardEntityProvider/service/ProviderResource.java
@@ -7,10 +7,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.StreamingOutput;
