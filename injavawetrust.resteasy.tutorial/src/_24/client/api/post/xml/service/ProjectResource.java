1463c04c2faf585917d68fb629418d90dd48497e
leventerguder <erguder.levent@gmail.com>
2017-08-20 21:16:15 +0300

    review

--- injavawetrust.resteasy.tutorial/src/_24/client/api/post/xml/service/ProjectResource.java
+++ injavawetrust.resteasy.tutorial/src/_24/client/api/post/xml/service/ProjectResource.java
@@ -1,12 +1,13 @@
 package _24.client.api.post.xml.service;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
 import _24.client.api.post.xml.model.Project;
-import _24.client.api.post.xml.model.ProjectList;
 
 @Path("/project-client-api")
 public class ProjectResource {
@@ -23,7 +24,7 @@ public class ProjectResource {
 	@Path("/addProjectBatch")
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
-	public String addProjectBatch(ProjectList projectList) {
+	public String addProjectBatch(List<Project> projectList) {
 		System.out.println("ProjectResource#addProjectBatch");
 		System.out.println(projectList);
 		return "addProjectBatch#succeed";
