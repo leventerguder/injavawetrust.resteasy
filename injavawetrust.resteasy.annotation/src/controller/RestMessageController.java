8697e78802aff75fd8d71340c14c07dc085e7812
leventerguder <erguder.levent@gmail.com>
2019-01-12 20:55:26 +0300

    update dependency

--- injavawetrust.resteasy.annotation/src/controller/RestMessageController.java
+++ injavawetrust.resteasy.annotation/src/controller/RestMessageController.java
@@ -8,6 +8,7 @@ public class RestMessageController {
 
 	@GET
 	@Path("/message")
+	// http://localhost:8080/injavawetrust.resteasy.annotation/root-path/messages/message
 	public String getMessage() {
 
 		String message = "Welcome to injavawetrust.com RESTEasy Tutorial !";
