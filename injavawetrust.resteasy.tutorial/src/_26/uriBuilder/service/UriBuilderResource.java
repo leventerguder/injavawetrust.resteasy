2257f974b97c377e6dcb30c17c7913b8f1e5d977
leventerguder <erguder.levent@gmail.com>
2019-03-30 16:45:12 +0300

    encodeSlashInPath example

--- injavawetrust.resteasy.tutorial/src/_26/uriBuilder/service/UriBuilderResource.java
+++ injavawetrust.resteasy.tutorial/src/_26/uriBuilder/service/UriBuilderResource.java
@@ -17,10 +17,7 @@ public class UriBuilderResource {
 	public String test1UriBuilder() {
 
 		UriBuilder builder = UriBuilder.fromPath("/customers/{id}");
-		builder.scheme("http")
-		.host("example.com")
-		.resolveTemplate("id", "100")
-		.queryParam("myParam", "myValue");
+		builder.scheme("http").host("example.com").resolveTemplate("id", "100").queryParam("myParam", "myValue");
 
 		URI uri = builder.build();
 		return uri.toString();
@@ -32,9 +29,7 @@ public class UriBuilderResource {
 	public String test2UriBuilder() {
 
 		UriBuilder builder = UriBuilder.fromPath("/customers/{id}");
-		builder.scheme("http")
-		.host("{hostname}")
-		.queryParam("{queryParam}", "{queryValue}");
+		builder.scheme("http").host("{hostname}").queryParam("{queryParam}", "{queryValue}");
 
 		URI uri = builder.build("example.com", "100", "myParam", "myValue");
 		return uri.toString();
@@ -46,9 +41,7 @@ public class UriBuilderResource {
 	public String test3UriBuilder() {
 
 		UriBuilder builder = UriBuilder.fromPath("/customers/{id}");
-		builder.scheme("http")
-		.host("{hostname}")
-		.queryParam("{queryParam}", "{queryValue}");
+		builder.scheme("http").host("{hostname}").queryParam("{queryParam}", "{queryValue}");
 
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("hostname", "example.com");
@@ -59,17 +52,43 @@ public class UriBuilderResource {
 		URI uri = builder.buildFromMap(map);
 		return uri.toString();
 	}
-	
+
 	@GET
 	@Path("/test4")
 	// http://localhost:8080/injavawetrust.resteasy.tutorial/uriBuilder-path/test4
 	public String test4UriBuilder() {
 
 		UriBuilder builder = UriBuilder.fromResource(CustomerResource.class);
-		builder.path(CustomerResource.class,"getCustomer");
-		
+		builder.path(CustomerResource.class, "getCustomer");
+
 		return builder.build().toString();
+
+	}
+
+	@GET
+	@Path("/test5")
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/uriBuilder-path/test5
+	public String test5UriBuilder() {
+
+		UriBuilder builder = UriBuilder.fromPath("/customers/{id}");
+		URI uri = builder.build("a/b");
+		return uri.toString();
+
+	}
+
+	@GET
+	@Path("/test6")
+	// http://localhost:8080/injavawetrust.resteasy.tutorial/uriBuilder-path/test6
+	public String test6UriBuilder() {
+
+		UriBuilder builder = UriBuilder.fromPath("/customers/{id}");
+		URI uri = builder.resolveTemplate("id", "a/b", false).build();
+		return uri.toString();
+
+		// public abstract UriBuilder resolveTemplate(String name, Object value, boolean
+		// encodeSlashInPath);
 		
+		// If you set the encodeSlashInPath to false, then the / character will not be encoded.
 	}
 
 }
