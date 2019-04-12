20d55da50f834189b359d7bb8fe3efb36a1889c4
leventerguder <erguder.levent@gmail.com>
2019-04-12 23:26:06 +0300

    RESTEasyBasicAuthentication example

---
+++ RESTEasyBasicAuthentication/src/resource/EmployeeResource.java
@@ -0,0 +1,22 @@
+package resource;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/employees")
+public class EmployeeResource {
+
+	@GET
+	public String getEmployees() {
+		return "dummy employee list";
+	}
+
+	@GET
+	@Path("{id}")
+	public String getUser(@PathParam("id") String id) {
+		return "dummy employee with id: " + id;
+	}
+}
